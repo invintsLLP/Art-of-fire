1c6e77578fd1a8b5188e7d75d3094c51a48c13ab
invintsLLP <jagrutivekariya261@gmail.com>
2021-09-20 14:50:21 +0530

    First

---
+++ art_of_fire_config.php
@@ -0,0 +1,6 @@
+<?php
+define("SHOPIFY_API_KEY","68cd401a089d0ab98b6408f6ec86f885");
+define("SHOPIFY_API_PASSWORD","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");
+define("API_DOMAIN","art-of-fire-shop.myshopify.com");
+define('API_URL', 'https://' . SHOPIFY_API_KEY . ':' . SHOPIFY_API_PASSWORD . '@' . API_DOMAIN . '/');
+?>
